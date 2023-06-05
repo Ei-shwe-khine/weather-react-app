d23f92aa31830f37d55619c656b06784930d63da
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-06-05 16:03:14 +0630

    added github url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        
+        This project was coded by <a href="https://www.shecodes.io/graduates/73819-ei-shwe-khine" target='_blank'>Ei Shwe Khine </a>{''} and is {''}
+        <a href='https://github.com/Ei-shwe-khine/weather-react-app' target='_blank'>open-sourced on GitHub</a>
+      
      </footer>
     </div>
   );

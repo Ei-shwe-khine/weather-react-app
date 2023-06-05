54ec3c59304bfa0456fc832b7d79f93255f067fb
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-06-05 18:00:18 +0630

    added npm bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
+      
       <footer>
         This project was coded by <a href="https://www.shecodes.io/graduates/73819-ei-shwe-khine" target='_blank'>Ei Shwe Khine </a>{''} and is {''}
         <a href='https://github.com/Ei-shwe-khine/weather-react-app' target='_blank'>open-sourced on GitHub</a>
       
      </footer>
-    </div>
+      </div>
+      </div>
   );
 }
 

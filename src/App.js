439499d00223230c93cb469636021db18790d14c
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-06-05 22:30:01 +0630

    added basic style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
       <h1>Weather App</h1>
-      
+        <Weather />
+        
       <footer>
         This project was coded by <a href="https://www.shecodes.io/graduates/73819-ei-shwe-khine" target='_blank'>Ei Shwe Khine </a>{''} and is {''}
         <a href='https://github.com/Ei-shwe-khine/weather-react-app' target='_blank'>open-sourced on GitHub</a>

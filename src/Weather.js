a5b08817ef4df7bb8bab31039f2022fc706d5d19
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-06-17 22:08:24 +0630

    updated api key

--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,13 @@ export default function Weather(props) {
   }
 
   function search() {
-   // const apiKey = "6655e6104636a644c9bc754d824a695d";
-        const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+   //const apiKey = "6655e6104636a644c9bc754d824a695d";
+  //const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    //const apiKey = "e82d170b1eef4883t30f30a2e29oab37";
+    const apiKey = "17ad6e67aa629189f73b053634668b20";
+   // const apiKey = "3499ef150985eccadd080ff408a018df";
+    //const apiKey = "76184a71275816c9e0d0b09aa75a7bcf";
+
 
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
@@ -62,15 +67,14 @@ export default function Weather(props) {
                 </div>
                
       <footer className="footer">
-        This project was coded by <a href="https://www.shecodes.io/graduates/73819-ei-shwe-khine" >Ei Shwe Khine </a>{''} and is {''}
-        <a href='https://github.com/Ei-shwe-khine/weather-react-app' >open-sourced on GitHub</a>
-       and <a href="https://648497e5d4b405000867f08c--cerulean-syrniki-ad1994.netlify.app/">hosted on Netlify</a>
+        This project was coded by <a href="https://www.shecodes.io/graduates/73819-ei-shwe-khine" target="_blank" rel="noopener noreferrer">Ei Shwe Khine </a>{''} and is {''}
+        <a href='https://github.com/Ei-shwe-khine/weather-react-app' target="_blank" rel="noopener noreferrer">open-sourced on GitHub </a>
+       and <a href="https://648497e5d4b405000867f08c--cerulean-syrniki-ad1994.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
      </footer>
                 </div>
         );
 
     } else {
-    //const apiKey = "6655e6104636a644c9bc754d824a695d";
     
         search();
         return "Loading.....";
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -7,13 +7,17 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+  
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
   if (loaded) {
-    return  (
+    return (
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
@@ -27,11 +31,16 @@ export default function WeatherForecast(props) {
           })}
         </div>
       </div>
-    );
+    );   
+     
   } else {
     
-    //let apiKey = "6655e6104636a644c9bc754d824a695d";
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+   // let apiKey = "6655e6104636a644c9bc754d824a695d";
+    //let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    //let apiKey = "e82d170b1eef4883t30f30a2e29oab37";
+    let apiKey = "17ad6e67aa629189f73b053634668b20";
+   // let apiKey = "3499ef150985eccadd080ff408a018df";
+   // let apiKey = "76184a71275816c9e0d0b09aa75a7bcf";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
